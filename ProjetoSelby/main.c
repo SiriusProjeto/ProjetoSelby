1bc86c28f8985c03c56bf4a8c459dc647e0fd9ba
JMelo <41171438+Josuka@users.noreply.github.com>
2018-12-04 19:03:05 -0200

    Add files via upload

--- ProjetoSelby/main.c
+++ ProjetoSelby/main.c
@@ -9,12 +9,16 @@
 //DECLARA C
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <allegro5/allegro.h>
 
 
 // CONSTANTES DO DISPLAY
 const int LARGURA_TELA = 1280;
 const int ALTURA_TELA = 720;
+
+//const int LARGURA_TELA = 960;
+//const int ALTURA_TELA = 540;
 
 
 //PARTES DO JOGO
@@ -36,11 +40,13 @@ ALLEGRO_FONT *fonte = NULL;
 ALLEGRO_BITMAP *x = NULL;
 ALLEGRO_COLOR cor_menu;
 ALLEGRO_COLOR cor_titulo;
+ALLEGRO_BITMAP *tela_inicio = NULL;
+ALLEGRO_BITMAP *explica_planeta[8] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+ALLEGRO_BITMAP *avaliacao_planeta[8] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
-
-//VARIAVEIS DE CONTROLE
+//VARIAVEIS DE CONTROLE PARA CADA TRANSIÇAO
 int controle = 0;
-
+char palavra[15];
 
 int inicializar(){
 	printf(">>> Iniciando o jogo...\n");
@@ -73,6 +79,7 @@ int inicializar(){
 		sucesso = 1;
 
         al_set_window_title(janela, "*SELBY SPACE*");// DEFINE NOME DA JANELA
+        //al_set_window_position(janela, 320, 0);
         al_register_event_source(fila_eventos, al_get_display_event_source(janela)); /*Registra os eventos que ocorrem dentro da janela*/
         al_register_event_source(fila_eventos, al_get_keyboard_event_source()); /*Registra os eventos do teclado*/
 
@@ -86,6 +93,74 @@ void troca_tela(ALLEGRO_BITMAP *img1, ALLEGRO_BITMAP *img2){
 	al_flip_display();
 	al_draw_bitmap(img2, 0, 0, 0);
 	al_flip_display();
+}
+
+/*int compara_palavra(char palavra[], int contador){
+    palavra = tolower(palavra);
+    if(palavra == "acre" && contador==0){
+        contador++;
+        return 0;
+    }
+    else if(palavra == "alagoas" && contador==1){
+        contador++;
+        return 0;
+    }
+}
+
+void carrega_estados(){
+    int i = 0;
+    al_draw_bitmap(estados[i], 0, 0, 0);
+        while(i <= 25){
+        compara_palavra(palavra, i);
+            if(compara_palavra(palavra, i) == 0){
+                al_draw_bitmap(estados[i], 0, 0, 0);
+                al_flip_display();
+            }
+
+    }
+
+}*/
+
+
+
+void mover_bitmap_em_x(ALLEGRO_BITMAP *bitmap, int x, int max){
+    if(x < max){
+        while(x <= max){
+            al_draw_bitmap(bitmap, x, 0, 0);
+            al_flip_display();
+            x = x + 100;
+            al_clear_to_color(al_map_rgb(0,0,0));
+        }
+    }
+    else if(x > max){
+        while(x >= max){
+            al_draw_bitmap(bitmap, x, 0, 0);
+            al_flip_display();
+            x = x - 100;
+            al_clear_to_color(al_map_rgb(0,0,0));
+        }
+    }
+}
+
+void intercala_tela(ALLEGRO_BITMAP *img1, ALLEGRO_BITMAP *img2, float tempo_de_trocar, int temporizador){
+    int mudanca = 1;
+    while(temporizador != 0){
+        if (temporizador > 0) temporizador--;
+            if(mudanca == 1){
+                al_draw_bitmap(img1, 0, 0, 0);
+                al_flip_display();
+                al_clear_to_color(al_map_rgb(0, 0, 0));
+                mudanca = mudanca * -1;
+                al_rest(tempo_de_trocar);
+            }
+            if(mudanca == -1){
+                al_draw_bitmap(img2, 0, 0, 0);
+                al_flip_display();
+                al_clear_to_color(al_map_rgb(0, 0, 0));
+                mudanca = mudanca * -1;
+                al_rest(tempo_de_trocar);
+        }
+    }
 }
 
 
@@ -155,8 +230,6 @@ void apresentacao(){
 
 void desenha_tela_inicio(){
 	printf(">>> Inicia função desenha tela inicio\n");
-	ALLEGRO_BITMAP *tela_inicio = NULL;
-
 	tela_inicio = al_load_bitmap("res/img/02_MENU/menu.bmp");
 	al_draw_bitmap(tela_inicio, 0, 0, 0);
 	al_flip_display();
@@ -218,10 +291,34 @@ int pega_tecla(){
 
 }
 
+/*void transicoes_tela(int i){
+    explica_planeta[0] = al_load_bitmap("res/img/transicoes_de_tela/transicaoMercurio.jpg");
+    explica_planeta[1] = al_load_bitmap("res/img/transicoes_de_tela/transicaoVenus.jpg");
+    explica_planeta[2] = al_load_bitmap("res/img/transicoes_de_tela/transicaoTerra.jpg");
+    explica_planeta[3] = al_load_bitmap("res/img/transicoes_de_tela/transicaoMarte.jpg");
+    explica_planeta[4] = al_load_bitmap("res/img/transicoes_de_tela/transicaoJupiter.jpg");
+    explica_planeta[5] = al_load_bitmap("res/img/transicoes_de_tela/transicaoSaturno.jpg");
+    explica_planeta[6] = al_load_bitmap("res/img/transicoes_de_tela/transicaoUrano.jpg");
+    explica_planeta[7] = al_load_bitmap("res/img/transicoes_de_tela/transicaoNetuno.jpg");
+
+    if(i < 8){
+    al_draw_bitmap(explica_planeta[i], 0, 0, 0);
+    i++;
+    }
+}*/
+
 
 void menu(){
-	printf(">>> Inicio função menu\n");
+//printf("f %d  ", fonte);
+    //fonte = al_load_font("res/font/comic.ttf", 42, 0);
+    //printf("f %d  ", fonte);
+	printf(">>> Inicio funcao menu\n");
+	//al_draw_text(word_font, al_map_rgb(0, 0, 0), LARGURA_TELA / 2, ALTURA_TELA / 2, ALLEGRO_ALIGN_LEFT, "tsst");
+	//al_flip_display();
 	desenha_tela_inicio();
+
+
+
 	{
 		if(pega_tecla() != 1){
 			controle++;
@@ -234,49 +331,121 @@ void menu(){
 	}
 }
 
-
-
-
 void boas_vindas(){
-	ALLEGRO_BITMAP *boasvindas[4] = {NULL,NULL,NULL};
+	ALLEGRO_BITMAP *boasvindas[4] = {NULL,NULL,NULL,NULL};
+	int i = 0;
 
-	boasvindas[0] = al_load_bitmap("res/img/03_BOASVINDAS/b1.bmp");
+    explica_planeta[0] = al_load_bitmap("res/img/transicoes_de_tela/transicaoMercurio.jpg");
+    explica_planeta[1] = al_load_bitmap("res/img/transicoes_de_tela/transicaoVenus.png");
+    explica_planeta[2] = al_load_bitmap("res/img/transicoes_de_tela/transicaoTerra.jpg");
+    explica_planeta[3] = al_load_bitmap("res/img/transicoes_de_tela/transicaoMarte.jpg");
+    explica_planeta[4] = al_load_bitmap("res/img/transicoes_de_tela/transicaoJupiter.jpg");
+    explica_planeta[5] = al_load_bitmap("res/img/transicoes_de_tela/transicaoSaturno.jpg");
+    explica_planeta[6] = al_load_bitmap("res/img/transicoes_de_tela/transicaoUrano.jpg");
+    explica_planeta[7] = al_load_bitmap("res/img/transicoes_de_tela/transicaoNetuno.jpg");
+
+    avaliacao_planeta[0] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_mercurio.jpg");
+    avaliacao_planeta[1] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_venus.jpg");
+    avaliacao_planeta[2] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_terra.jpg");
+    avaliacao_planeta[3] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_marte.jpg");
+    avaliacao_planeta[4] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_jupiter.jpg");
+    avaliacao_planeta[5] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_saturno.jpg");
+    avaliacao_planeta[6] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_urano.jpg");
+    avaliacao_planeta[7] = al_load_bitmap("res/img/transicoes_de_tela/avaliacao_netuno.jpg");
+
+	/*boasvindas[0] = al_load_bitmap("res/img/03_BOASVINDAS/b1.bmp");
 	boasvindas[1] = al_load_bitmap("res/img/03_BOASVINDAS/b2.bmp");
 	boasvindas[2] = al_load_bitmap("res/img/03_BOASVINDAS/b3.bmp");
 	boasvindas[3] = al_load_bitmap("res/img/03_BOASVINDAS/b4.bmp");
 
-	al_flip_display();
 	al_draw_bitmap(boasvindas[0], 0, 0, 0);
-	al_rest(6.0);
-
 	al_flip_display();
-	al_draw_bitmap(boasvindas[1], 0, 0, 0);
+	al_rest(1.0);
+
+	intercala_tela(boasvindas[1], boasvindas[2], 0.5, 5);
 	al_destroy_bitmap(boasvindas[0]);
-	al_rest(6.0);
 
-	al_flip_display();
-	al_draw_bitmap(boasvindas[2], 0, 0, 0);
-	al_destroy_bitmap(boasvindas[1]);
-	al_rest(3.0);
-
-	al_flip_display();
 	al_draw_bitmap(boasvindas[3], 0, 0, 0);
-	al_destroy_bitmap(boasvindas[2]);
-	al_rest(3.0);
-
-
 	al_flip_display();
-	al_draw_bitmap(boasvindas[4], 0, 0, 0);
+	al_rest(1.0);
+
+	al_destroy_bitmap(boasvindas[1]);
+	al_destroy_bitmap(boasvindas[2]);*/
+
+
+	//outras transicoes
+	al_draw_bitmap(explica_planeta[0], 0, 0, 0);
 	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[0], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[0]);
 
-	al_rest(7.0);
+    al_draw_bitmap(explica_planeta[1], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[1], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[1]);
+
+    al_draw_bitmap(explica_planeta[2], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[2], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[2]);
+
+    al_draw_bitmap(explica_planeta[3, 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[3], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[3]);
+
+    al_draw_bitmap(explica_planeta[4], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[4], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[4]);
+
+    al_draw_bitmap(explica_planeta[5], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[5], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[5]);
+
+    al_draw_bitmap(explica_planeta[6], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[6], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[6]);
+
+    al_draw_bitmap(explica_planeta[7], 0, 0, 0);
+	al_flip_display();
+	al_rest(15.0);
+	mover_bitmap_em_x(avaliacao_planeta[7], 0, -2560);
+	al_destroy_bitmap(avaliacao_planeta[7]);
 
 
+    /*while(i < 8){
+    al_draw_bitmap(explica_planeta[i], 0, 0, 0);
 
-	controle++;
+        if(pega_tecla() != 1){
+			i++;
+			al_clear_to_color(al_map_rgb(0,0,0));
+        }
+    }*/
+	//al_destroy_bitmap(boasvindas[3]);
+
+	//al_rest(7.0);
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+
+    //transicoes_tela(i);
+	//controle++;
 }
 
 
+
+/*void jogo(){
+    int i =0;
+    transicoes_tela(i);
+}*/
 
 
 
@@ -287,7 +456,6 @@ int main(){
 		return -1;
 	}
 
-
 	while(controle != P_FINAL){
 		if(controle == P_APRESENTACAO){
 			apresentacao();
@@ -296,6 +464,7 @@ int main(){
 		} else if(controle == P_BOASVINDAS){
 			boas_vindas();
 		} else if(controle == P_TRANSICAO_NIVEL_1){
+            //jogo();
 			printf("OKOK\n");
 			break;
 		}
@@ -306,4 +475,6 @@ int main(){
 	destroy();
 	return 0;
 }
+
+
 
